73fdd887cc3942accbe4a14099d9ef4ca5383967
Amar Gill <amardeep@clearbanc.com>
2023-06-25 15:09:22 -0400

    03 part 2 completed

--- 03/part2.go
+++ 03/part2.go
@@ -4,8 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
+
+const GROUP_SIZE = 3
 
 func main() {
 	priorities := []int{}
@@ -17,18 +18,18 @@ func main() {
 
 	scanner := bufio.NewScanner(f)
 
+	rucksacks := []string{}
 	for scanner.Scan() {
 		text := scanner.Text()
-		length := len(text)
-		compartment1 := text[0 : length/2]
-		compartment2 := text[length/2 : length]
-
-		for _, v := range compartment1 {
-			containsRune := strings.ContainsRune(compartment2, v)
-
-			if containsRune {
-				priorities = append(priorities, convertRuneToPriority(v))
-				break
+		rucksacks = append(rucksacks, text)
+		if len(rucksacks) == GROUP_SIZE {
+			runeSet := initializeRuneSet(rucksacks[0])
+			runeSet = updateRuneSet(rucksacks[1], runeSet, 1)
+			runeSet = updateRuneSet(rucksacks[2], runeSet, 2)
+			rucksacks = rucksacks[:0]
+			// should be map length 1 at this point
+			for k := range runeSet {
+				priorities = append(priorities, convertRuneToPriority(k))
 			}
 		}
 	}
@@ -37,7 +38,39 @@ func main() {
 	for _, v := range priorities {
 		sum += v
 	}
-	fmt.Println(sum)
+	fmt.Println(sum) // 2716
+}
+
+func initializeRuneSet(s string) map[rune]int {
+	ret := map[rune]int{}
+
+	for _, v := range s {
+		if _, ok := ret[v]; !ok {
+			ret[v] = 0
+		}
+	}
+	return ret
+}
+
+func updateRuneSet(s string, m map[rune]int, iteration int) map[rune]int {
+	ret := m
+
+	for _, v := range s {
+		if _, ok := ret[v]; !ok {
+			delete(ret, v)
+		} else if ret[v] == iteration {
+			continue
+		} else {
+			ret[v]++
+		}
+	}
+
+	for k, v := range ret {
+		if v != iteration {
+			delete(ret, k)
+		}
+	}
+	return ret
 }
 
 // Lowercase item types a through z have priorities 1 through 26.
