37ff060e303d18ff79f18b1b174c1dd86a96ee2d
Amar Gill <amardeep@clearbanc.com>
2023-06-25 18:02:34 -0400

    04 cp part1 for part2

---
+++ 04/part2.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.in")
+	if err != nil {
+		panic("omg")
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	numPairs := 0
+	for scanner.Scan() {
+		text := scanner.Text()
+		vals := strings.Split(text, ",")
+		elf1Range := strings.Split(vals[0], "-")
+		elf2Range := strings.Split(vals[1], "-")
+
+		elf1Min, _ := strconv.Atoi(elf1Range[0])
+		elf1Max, _ := strconv.Atoi(elf1Range[1])
+
+		elf2Min, _ := strconv.Atoi(elf2Range[0])
+		elf2Max, _ := strconv.Atoi(elf2Range[1])
+
+		if elf1Min >= elf2Min && elf1Max <= elf2Max {
+			numPairs++
+		} else if elf2Min >= elf1Min && elf2Max <= elf1Max {
+			numPairs++
+		}
+	}
+	fmt.Println(numPairs) // 456
+}
