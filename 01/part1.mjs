9035b4188a7cde2ea59bfda346680112edd37323
Amar Gill <amardeep@clearbanc.com>
2023-07-08 12:58:46 -0400

    01 - part1 in js

--- 01/part1.mjs
+++ 01/part1.mjs
@@ -2,6 +2,20 @@ import { open } from "node:fs/promises";
 
 const file = await open("./input.in");
 
+let runningSum = 0;
+let max = 0;
+
 for await (const line of file.readLines()) {
-  console.log(line);
+  const v = parseInt(line);
+
+  if (!isNaN(v)) {
+    runningSum += v;
+  }
+
+  if (line.length === 0) {
+    max = Math.max(runningSum, max);
+    runningSum = 0;
+  }
 }
+
+console.log(max);
