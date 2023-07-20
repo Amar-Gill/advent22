715dee17f057e223376c0dae6f50ba5967f0ec32
Amar Gill <amardeep@clearbanc.com>
2023-07-20 18:49:57 -0400

    06 - completed part1 (go)

--- 06/part1.go
+++ 06/part1.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+var markerLength = 4
+
 func main() {
 	f, err := os.Open("input.in")
 	if err != nil {
@@ -16,11 +18,45 @@ func main() {
 
 	reader := bufio.NewReader(f)
 
+	sequence := []rune{}
+	charCount := -1
 	for {
+		charCount++
+
 		r, _, err := reader.ReadRune()
 		if err == io.EOF {
 			break
 		}
-		fmt.Println(r)
+
+		if len(sequence) < markerLength {
+			sequence = append(sequence, r)
+			continue
+		}
+
+		runeSet := buildRuneSet(sequence)
+
+		if len(runeSet) == markerLength {
+			// means we have "markerLength" unique runes in the set, so we found the marker
+			break
+		}
+
+		sequence = sequence[1:]
+		sequence = append(sequence, r)
 	}
+
+	fmt.Println(charCount)
+}
+
+func buildRuneSet(runes []rune) map[rune]int {
+	ret := map[rune]int{}
+
+	for _, r := range runes {
+		if _, ok := ret[r]; !ok {
+			ret[r] = 1
+			continue
+		}
+		ret[r]++
+	}
+
+	return ret
 }
